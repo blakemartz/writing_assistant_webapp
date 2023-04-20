1c8909b7ae4881fe15a38cda6fdd9f2b232fcacb
Blake Martz <blakemartz@gmail.com>
2023-04-19 23:55:14 -0700

    Add new demo samples to demo function
    change array in demoSamples.js
    
    import demoSamplesArray into js module in imdex.html
    
    add random function to choose from different demo samples

--- static/demoSamples.js
+++ static/demoSamples.js
@@ -1,4 +1,5 @@
-const predefinedSamples = {
+const demoSamplesArray = [
+    {
     sample1: `Come gather 'round people
 Wherever you roam
 And admit that the waters
@@ -179,4 +180,313 @@ So take heed, take heed of the western wind
 Take heed of the stormy weather
 And yes, there's something you can send back to me
 Spanish boots of Spanish leather.`,
-};
+}, 
+
+{
+    sample1: `Suzanne takes you down to her place near the river
+You can hear the boats go by
+You can spend the night beside her
+And you know that she's half crazy
+But that's why you want to be there
+And she feeds you tea and oranges
+That come all the way from China
+And just when you mean to tell her
+That you have no love to give her
+Then she gets you on her wavelength
+And she lets the river answer
+That you've always been her lover
+And you want to travel with her
+And you want to travel blind
+And you know that she will trust you
+For you've touched her perfect body with your mind.
+And Jesus was a sailor
+When he walked upon the water
+And he spent a long time watching
+From his lonely wooden tower
+And when he knew for certain
+Only drowning men could see him
+He said "All men will be sailors then
+Until the sea shall free them"
+But he himself was broken
+Long before the sky would open
+Forsaken, almost human
+He sank beneath your wisdom like a stone
+And you want to travel with him
+And you want to travel blind
+And you think maybe you'll trust him
+For he's touched your perfect body with his mind.
+
+Now Suzanne takes your hand
+And she leads you to the river
+She is wearing rags and feathers
+From Salvation Army counters
+And the sun pours down like honey
+On our lady of the harbour
+And she shows you where to look
+Among the garbage and the flowers
+There are heroes in the seaweed
+There are children in the morning
+They are leaning out for love
+And they will lean that way forever
+While Suzanne holds the mirror
+And you want to travel with her
+And you want to travel blind
+And you know that you can trust her
+For she's touched your perfect body with her mind.`,
+
+    sample2: `It's four in the morning, the end of December
+I'm writing you now just to see if you're better
+New York is cold, but I like where I'm living
+There's music on Clinton Street all through the evening.
+I hear that you're building your little house deep in the desert
+You're living for nothing now, I hope you're keeping some kind of record.
+
+Yes, and Jane came by with a lock of your hair
+She said that you gave it to her
+That night that you planned to go clear
+Did you ever go clear?
+
+Ah, the last time we saw you you looked so much older
+Your famous blue raincoat was torn at the shoulder
+You'd been to the station to meet every train
+And you came home without Lili Marlene
+
+And you treated my woman to a flake of your life
+And when she came back she was nobody's wife.
+
+Well I see you there with the rose in your teeth
+One more thin gypsy thief
+Well I see Jane's awake --
+
+She sends her regards.
+
+And what can I tell you my brother, my killer
+What can I possibly say?
+I guess that I miss you, I guess I forgive you
+I'm glad you stood in my way.
+
+If you ever come by here, for Jane or for me
+Your enemy is sleeping, and his woman is free.
+
+Yes, and thanks, for the trouble you took from her eyes
+I thought it was there for good so I never tried.
+
+And Jane came by with a lock of your hair
+She said that you gave it to her
+That night that you planned to go clear --`,
+
+    sample3: `I believe that you heard your master sing
+when I was sick in bed.
+I suppose that he told you everything
+that I keep locked away in my head.
+Your master took you travelling,
+well at least that's what you said.
+And now do you come back to bring
+your prisoner wine and bread?
+You met him at some temple, where
+they take your clothes at the door.
+He was just a numberless man in a chair
+who'd just come back from the war.
+And you wrap up his tired face in your hair
+and he hands you the apple core.
+Then he touches your lips now so suddenly bare
+of all the kisses we put on some time before.
+
+And he gave you a German Shepherd to walk
+with a collar of leather and nails,
+and he never once made you explain or talk
+about all of the little details,
+such as who had a word and who had a rock,
+and who had you through the mails.
+Now your love is a secret all over the block,
+and it never stops not even when your master fails.
+
+And he took you up in his aeroplane,
+which he flew without any hands,
+and you cruised above the ribbons of rain
+that drove the crowd from the stands.
+Then he killed the lights in a lonely Lane
+and, an ape with angel glands,
+erased the final wisps of pain
+with the music of rubber bands.
+
+And now I hear your master sing,
+you kneel for him to come.
+His body is a golden string
+that your body is hanging from.
+His body is a golden string,
+my body has grown numb.
+Oh now you hear your master sing,
+your shirt is all undone.
+
+And will you kneel beside this bed
+that we polished so long ago,
+before your master chose instead
+to make my bed of snow?
+Your eyes are wild and your knuckles are red
+and you're speaking far too low.
+No I can't make out what your master said
+before he made you go.
+
+Then I think you're playing far too rough
+for a lady who's been to the moon;
+I've lain by this window long enough
+to get used to an empty room.
+And your love is some dust in an old man's cough
+who is tapping his foot to a tune,
+and your thighs are a ruin, you want too much,
+let's say you came back some time too soon.
+
+I loved your master perfectly
+I taught him all that he knew.
+He was starving in some deep mystery
+like a man who is sure what is true.
+And I sent you to him with my guarantee
+I could teach him something new,
+and I taught him how you would long for me
+no matter what he said no matter what you'd do.
+
+I believe that you heard your master sing
+while I was sick in bed,
+I'm sure that he told you everything
+I must keep locked away in my head.
+Your master took you travelling,
+well at least that's what you said,
+And now do you come back to bring
+your prisoner wine and bread?`
+}, 
+
+
+{
+    sample1: `Well, I dreamed I saw the knights in armor coming,
+Saying something about a queen.
+There were peasants singing and drummers drumming,
+And the archer split the tree.
+
+There was a fanfare blowing to the sun
+That was floating on the breeze.
+
+Look at Mother Nature on the run
+In the nineteen seventies.
+Look at Mother Nature on the run
+In the nineteen seventies.
+
+I was lying in a burned out basement
+With the full moon in my eyes.
+I was hoping for replacement
+When the sun burst through the sky.
+
+There was a band playing in my head,
+And I felt like getting high.
+
+I was thinking about what a friend had said.
+I was hoping it was a lie.
+Thinking about what a friend had said.
+I was hoping it was a lie.
+
+Well, I dreamed I saw the silver space ships flying
+In the yellow haze of the sun.
+There were children crying and colors flying
+All around the chosen ones.
+
+All in a dream, all in a dream
+The loading had begun.
+
+Flying Mother Nature's silver seed to a new home in the sun.
+Flying Mother Nature's silver seed to a new home.`, 
+ 
+    sample2: `Old man look at my life,
+I'm a lot like you were.
+Old man look at my life,
+I'm a lot like you were.
+
+Old man look at my life,
+Twenty four
+and there's so much more
+Live alone in a paradise
+That makes me think of two.
+
+Love lost, such a cost,
+Give me things
+that don't get lost.
+Like a coin that won't get tossed
+Rolling home to you.
+
+Old man take a look at my life
+I'm a lot like you
+I need someone to love me
+the whole day through
+Ah, one look in my eyes
+and you can tell that's true.
+
+Lullabies, look in your eyes,
+Run around the same old town.
+Doesn't mean that much to me
+To mean that much to you.
+
+I've been first and last
+Look at how the time goes past.
+But I'm all alone at last.
+Rolling home to you.
+
+Old man take a look at my life
+I'm a lot like you
+I need someone to love me
+the whole day through
+Ah, one look in my eyes
+and you can tell that's true.
+
+Old man look at my life,
+I'm a lot like you were.
+Old man look at my life,
+I'm a lot like you were.`, 
+ 
+    sample3: `When you were young
+and on your own
+How did it feel
+to be alone?
+I was always thinking
+of games that I was playing.
+Trying to make
+the best of my time.
+
+But only love
+can break your heart
+Try to be sure
+right from the start
+Yes only love
+can break your heart
+What if your world
+should fall apart?
+
+I have a friend
+I've never seen
+He hides his head
+inside a dream
+Someone should call him
+and see if he can come out.
+Try to lose
+the down that he's found.
+
+But only love
+can break your heart
+Try to be sure
+right from the start
+Yes only love
+can break your heart
+What if your world
+should fall apart?
+
+I have a friend
+I've never seen
+He hides his head
+inside a dream
+Yes, only love
+can break your heart
+Yes, only love
+can break your heart`
+}
+
+];
+
+export { demoSamplesArray };
