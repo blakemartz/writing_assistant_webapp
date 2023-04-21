04014e34c6c2e2fedce1e3d72198c583f3f331d3
Blake Martz <blakemartz@gmail.com>
2023-04-21 14:12:16 -0700

    Update welcome message

--- welcome.py
+++ welcome.py
@@ -1,3 +1,3 @@
 WELCOME = ["""Paste at least three sample texts in the fields.""", 
-           """echoScribe will attempt to capture your writing style and create an entirely new text written in your style.""", 
-           """Press submit to generate. Press again to regenerate."""]
+           """The echoScribe AI agent will attempt to capture your writing style and create an entirely new text written in your style.""", 
+           """Press Submit to generate. Press again to regenerate."""]
