aa4ea631ecdb3d687ad5d46306e63bbf6da77a27
Blake Martz <blakemartz@gmail.com>
2023-04-25 13:04:13 -0700

    Update welcome messages
    Add Demo instructions in welcome.py

--- welcome.py
+++ welcome.py
@@ -1,3 +1,3 @@
 WELCOME = ["""Paste at least three sample texts in the fields.""", 
            """The echoScribe AI agent will attempt to capture your writing style and create an entirely new text written in your style.""", 
-           """Press Submit to generate. Press again to regenerate."""]
+           """Press Submit to generate. Press again to regenerate. Press Demo to see echoScribe at work."""]
