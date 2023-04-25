6ca5cb117e66203a8382081ba9f3cb13e3252a21
Blake Martz <blakemartz@gmail.com>
2023-04-25 12:36:21 -0700

    Hide Subject Button
    Add hidden class to subject line in index.html
    Outputs with subject have poor imitation quality
    
    Simplify WELCOME variable handling in app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ from welcome import WELCOME
 
 app = Flask(__name__)
 
-PLACEHOLDER = WELCOME
-
 @app.route('/')
 def home():
     num_text_inputs = 3
-    specific_texts = [PLACEHOLDER[0], PLACEHOLDER[1], PLACEHOLDER[2]]
+    specific_texts = [WELCOME[0], WELCOME[1], WELCOME[2]]
     return render_template('index.html', num_text_inputs=num_text_inputs, specific_texts=specific_texts)
 
 @app.route('/submit', methods=['POST'])
